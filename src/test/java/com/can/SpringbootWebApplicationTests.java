c1ade6391e01734826b11748e7a8e344e3edbb01
uyu <767811890@qq.com>
2021-01-06 15:34:57 +0800

    v1.0原版

--- src/test/java/com/can/SpringbootWebApplicationTests.java
+++ src/test/java/com/can/SpringbootWebApplicationTests.java
@@ -1,6 +1,10 @@
 package com.can;
 
+import com.can.mapper.EmployeeMapper;
+import com.can.pojo.Employee;
+import com.can.service.EmployeeService;
 import com.can.service.impl.DepartmentServiceImpl;
+import com.dangdang.ddframe.rdb.sharding.id.generator.IdGenerator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,11 +12,24 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class SpringbootWebApplicationTests {
     @Autowired
-    DepartmentServiceImpl userService;
-
+    EmployeeMapper employeeMapper;
+    @Autowired
+    private IdGenerator idGenerator;
     @Test
     void contextLoads(){
-        System.out.println(userService.getDepartments());
+        long[] a = new long[10];
+        for (int i=0; i<10; i++) {
+            a[i] = idGenerator.generateId().longValue();
+        }
+        for (int i=0; i<10; i++) {
+            Employee employee = new Employee();
+            employee.setId(a[i]);
+            employee.setDepartmentName("教学楼" + (i%6 + 1));
+            employee.setEmail("test"+ i +"@.com");
+            employee.setGender(i%2);
+            employee.setLastName("test" + i);
+            employeeMapper.putEmployee(employee);
+        }
     }
 
 }
